a085811f1a3c02ef3677dbc3cc09197e3915876a
bernat-ferrer <bernatferrerm@gmail.com>
2021-02-26 16:04:31 +0100

    add list ingredients feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,9 +8,8 @@ router = DefaultRouter()
 
 router.register('tags', views.TagViewSet)
 
+router.register('ingredients', views.IngredientViewSet)
 app_name = 'recipe'
-
 urlpatterns = [
-
     path('', include(router.urls))
 ]
