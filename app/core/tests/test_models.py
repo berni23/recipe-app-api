e1d63c2f534976b6112b1939bbd67fbc988118ba
bernat-ferrer <bernatferrerm@gmail.com>
2021-02-26 18:13:15 +0100

    upgrade python version in travis.yml

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -71,10 +71,21 @@ class ModelTests(TestCase):
     def test_ingredient_str(self):
         """ Test the ingredient string representation"""
         ingredient = models.Ingredient.objects.create(
-
-
             user=sample_user(),
             name='Cucumber'
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """ Test the recipe string representation"""
+
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
