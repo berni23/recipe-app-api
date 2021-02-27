8d1e8fbe0f627cf73bd9ec66cb3dd15b90ea376e
bernat-ferrer <bernatferrerm@gmail.com>
2021-02-27 13:18:23 +0100

    added image field to recipe model

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
 
@@ -23,5 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
-]
-#
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# + static -> it makes the media url available in our development server, so we
+# do not have to set up a separate server
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,7 @@
+
+import uuid
+import os
+
 from django.db import models
 
 
@@ -6,6 +10,14 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 # recommended way to retrieve auth usermodel
 from django.conf import settings
+
+
+def recipe_image_file_path(instance, filename):
+    """ Generate file path for new recipe image"""
+
+    ext = filename.split('.')[-1]  # get extension filename
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('uploads/recipe', filename)
 
 
 class UserManager(BaseUserManager):
@@ -89,9 +101,9 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
-
     ingredients = models.ManyToManyField('Ingredient')
     tags = models.ManyToManyField('Tag')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
