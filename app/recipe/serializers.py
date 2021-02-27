cd40e0e23e28d153a45b4472c80774cadb7001e3
bernat-ferrer <bernatferrerm@gmail.com>
2021-02-27 11:09:34 +0100

    added list feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Ingredient, Tag
+from core.models import Ingredient, Tag, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -9,14 +9,39 @@ class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ('id', 'name')
-        read_only_field = ('id',)
+
+        # prevent user from modifying the primary key
+        read_only_fields = ('id',)
 
 
 class IngredientSerializer(serializers.ModelSerializer):
     """ Serializer for ingredient objects"""
 
     class Meta:
-
         model = Ingredient
         fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """ Serializer for recipe object """
+
+    # primary key related field
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+
+    tags = serializers.PrimaryKeyRelatedField(
+
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+
+        model = Recipe
+        fields = ('id', 'title', 'ingredients', 'tags',
+                  'time_minutes', 'price', 'link')
+
         read_only_fields = ('id',)
